cd9aa254df9b7a38028c0b4abe967a5f9d43f398
Vidushi <garg.vid@husky.neu.edu>
2019-11-06 17:26:25 -0500

    editing config

--- infrastructure/aws/terraform/application/variables.tf
+++ infrastructure/aws/terraform/application/variables.tf
@@ -1,6 +1,7 @@
 variable "vpc-cluster-name" {
   description = "This name will be used for all the resources created in AWS"
   type    = "string"
+  default = "vpc-circleci-one"
 }
 
 variable "VPC_ciderBlock"{
@@ -12,40 +13,52 @@ variable "VPC_ciderBlock"{
 variable "ami_name" {
 description = "AMI Name to be used for EC2 Security group creation"
   type    = "string"
+  default = "csye6225_1571787330" 
 }
 
 variable "ami_id" {
 description = "AMI Id to be used for EC2 Security group creation"
   type    = "string"
+    default="ami-0f1a3c4e46c3d59c5"
+
 }
 
 variable "ami_key_pair_name" {
 description = "AMI Key-Pair Name to be used for EC2 Security group creation"
   type    = "string"
+    default="csye6225_key"
+
 }
 
 variable "instance_type"{
 description = "Instance Type to be used for EC2 Instance creation"
   type    = "string"
+    default="t2.micro"
+
 }
 
 variable "volume_size"{
   description = "Instance Volume Size to be used for EC2 Instance creation"
   type    = "string"
+  default="20"
 }
 
 variable "volume_type"{
   description = "Instance Volume Type to be used for EC2 Instance creation"
   type    = "string"
+  default="gp2"
 }
 
 variable "domain-name"{
   description = "Domain Name to be used for S3 bucket creation"
   type    = "string"
+  default="dev.vidushigarg.me"
 }
-
-variable "region"{
-   description = "Region to be used for S3 bucket and VPC creation"
+variable "account_id"{
   type    = "string"
-default="us-east-2"
+  default="433434323910"
+}
+variable "region"{
+  type    = "string"
+  default="us-east-2"
 }
