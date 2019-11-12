7115ac1a808c92a259f2a7cb1caebbf75c392961
MrinalRai <rai.mr@northeastern.edu>
2019-11-11 21:12:35 -0500

    jk

--- infrastructure/aws/terraform/application/csye6225-application.tf
+++ infrastructure/aws/terraform/application/csye6225-application.tf
@@ -179,7 +179,7 @@ resource "aws_kms_key" "key" {
 	description = "This key is used to encrypt bucket objects"
 	deletion_window_in_days = 10
 }
-#Creating S3 Buckets
+#Creating S3 Bucket for codedeploy
 resource "aws_s3_bucket" "bucket" {
 	bucket = "codedeploy.${var.domain-name}"
 	acl    = "private"
