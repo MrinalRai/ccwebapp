70e9550c190f14bcd25865f42c61c2126ef62e8f
Vidushi <garg.vid@husky.neu.edu>
2019-11-13 12:39:14 -0500

    Adding RDS

--- infrastructure/aws/codedeploy/appStart.sh
+++ infrastructure/aws/codedeploy/appStart.sh
@@ -2,6 +2,7 @@
 # sudo systemctl daemon-reload
 # sudo systemctl enable tomcat
 # sudo systemctl restart tomcat.service
+cd ~
 sudo chmod 777 Cloud*.war
 # cd /
 java -jar CloudAssignment2-0.0.1-SNAPSHOT.war
