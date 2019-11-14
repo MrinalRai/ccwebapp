1c8da33c26f6d06b9e0eee8de6d9a9f9de763a75
Vidushi <garg.vid@husky.neu.edu>
2019-11-13 20:50:56 -0500

    refreshed timeout values

--- infrastructure/aws/codedeploy/appStart.sh
+++ infrastructure/aws/codedeploy/appStart.sh
@@ -2,8 +2,9 @@
 
 cd ~
 
-#sudo chmod 777 /opt/tomcat/bin/
+sudo chmod 777 /opt/tomcat/bin/
 #sudo chmod 777 /opt/tomcat/conf/Catalina
-#sudo chmod 777 /opt/tomcat/webapps/
+sudo chmod 777 /opt/tomcat/webapps/
 # cd /
+sudo chmod 777 /opt/tomcat/logs/
 java -jar CloudAssignment2-0.0.1-SNAPSHOT.war
