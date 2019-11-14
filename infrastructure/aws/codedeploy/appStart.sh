9c2c45e55747c14b5b3ca211997eceaa7fd444f8
Vidushi <garg.vid@husky.neu.edu>
2019-11-13 19:41:01 -0500

    refreshed db values

--- infrastructure/aws/codedeploy/appStart.sh
+++ infrastructure/aws/codedeploy/appStart.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 # sudo systemctl daemon-reload
-
 # sudo systemctl restart tomcat.service
 cd ~
 
-sudo chmod 777 /opt/tomcat/bin/
-sudo chmod 777 /opt/tomcat/conf/Catalina
-sudo chmod 777 /opt/tomcat/webapps/
+#sudo chmod 777 /opt/tomcat/bin/
+#sudo chmod 777 /opt/tomcat/conf/Catalina
+#sudo chmod 777 /opt/tomcat/webapps/
 # cd /
 java -jar CloudAssignment2-0.0.1-SNAPSHOT.war
