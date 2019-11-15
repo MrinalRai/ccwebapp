fb45145b7a1738161ea2d780967394b6777a569e
MrinalRai <rai.mr@northeastern.edu>
2019-11-15 18:03:54 -0500

    metrics check

--- webapp/CloudAssignment2/src/main/java/com/neu/config/MetricsConfig.java
+++ webapp/CloudAssignment2/src/main/java/com/neu/config/MetricsConfig.java
@@ -1,9 +1,12 @@
 package com.neu.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.neu.controller.UserController;
 import com.timgroup.statsd.NoOpStatsDClient;
 import com.timgroup.statsd.NonBlockingStatsDClient;
 import com.timgroup.statsd.StatsDClient;
@@ -18,11 +21,14 @@ public class MetricsConfig {
 
 	@Value("${metrics.server.port}")
 	private int metricsServerPort;
+	
+	private final static Logger logger = LoggerFactory.getLogger(MetricsConfig.class);
 
 	@Bean
 	public StatsDClient metricsClient() {
 
 		if (publishMetrics) {
+			logger.debug("Inside metrics client");
 			return new NonBlockingStatsDClient("csye6225", metricsServerHost, metricsServerPort);
 		}
 
